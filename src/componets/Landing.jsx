d03092be4d3981a3421f5812bab5b75045164b6c
Eduardo Laredo <eduardo.fco.laredo.mateos@gmail.com>
2023-02-09 21:43:22 -0800

    landing

--- src/componets/Landing.jsx
+++ src/componets/Landing.jsx
@@ -1,30 +1,50 @@
-import NavBar from "./NavBar";
-import Badge from "react-bootstrap/Badge";
-import { Container, Image } from "react-bootstrap";
-import perfil from "../img/profile.jpg";
+
+import vector from "../img/vector.png";
+import style from "./Landing.module.css";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
     <>
-      <NavBar />
-
-      <Container className="d-flex p-2">
-        <Image src={perfil} className="img-fluid w-50 rounded-circle shadow-sm" />
-        <Container className="col-lg-7 text-center " >
-          <h3 className="text white font-weight-normal mb-3">
-            Im
-          </h3>
-          <h1
-              className="display-3 text-uppercase text-primary mb-2" >
-              Eduardo Laredo
-            </h1>
-            <Container className="typed-text d-none">
+      <header className={style.header}>
+        <div className={style.container}>
+          <nav>
+          <Link to="">Menu</Link>
+            {/* <div className="links one" id="links">
+              <Link to="./home">Home</Link>
+              <Link to="./about">About</Link>
+              <Link to="./skills">Skills</Link>
+              <Link to="./projects">Projects</Link>
+              <Link to="./Contact">Contact</Link>
+            </div> */}
+          </nav>
+          <div className={style.text}>
+            <h1>Eduardo</h1>
+            <h2>Full Stack Developer</h2>
+            <div className={style.animacion}>
               HTML - CSS - React - Redux, API Rest - Node Js - Express ,
               Sequelize - Mongoose - Git , SQLite - PostgreSql - MongoDb, Git
               Flow - JSON Web Token (JWT)
-            </Container>
-        </Container>
-      </Container>
+            </div>
+          </div>
+          <img className={style.img} src={vector} alt="img vector" />
+        </div>
+      </header>
+
+      <div className={style.wave}>
+        <div style={{ height: "150px", overflow: "hidden" }}>
+          <svg
+            viewBox="0 0 500 150"
+            preserveAspectRatio="none"
+            style={{ height: "100%", width: "100%" }}
+          >
+            <path
+              d="M-0.00,49.85 C149.77,156.98 282.39,-93.99 500.00,49.85 L500.00,149.60 L-2.03,150.09 Z"
+              style={{ stroke: "none", fill: "#fff" }}
+            ></path>
+          </svg>
+        </div>
+      </div>
     </>
   );
 };
